7248e5ce769d8b5bfe5db64f7bc1a0bdfb0aed93
Theo-Selin <theo.selin@hotmail.com>
2022-11-22 15:57:09 +0100

    Styling and assets

--- client/src/views/Contact.vue
+++ client/src/views/Contact.vue
@@ -1,32 +1,24 @@
 <template>
   <main>
     <section
-      class="fixed left-0 right-0 top-36 mx-auto flex flex-col gap-10 h-screen max-w-[1350px] items-center md:top-64"
+      class="fixed left-0 right-0 top-10 mx-auto flex flex-col-reverse h-screen max-w-[1350px] items-center justify-evenly md:justify-center md:flex-row md:top-0"
     >
+      <div
+        class="relative h-[450] w-[300px] transition-all duration-500 md:inline lg:h-[500px] lg:w-[500px]"
+      >
+        <img src="../assets/elephants.png" layout="fill" objectFit="contain" />
+      </div>
       <div>
         <h1
-          class="space-y-8 text-5xl font-semibold tracking-wide lg:text-7xl xl:text-7xl"
+          class="space-y-3 text-5xl font-semibold tracking-wide lg:text-7xl xl:text-7xl"
         >
-          <span
-            class="block bg-gradient-to-r from-violet-900 to-pink-500 bg-clip-text text-transparent"
-          >
-            I
-          </span>
+          <span class="block"> I </span>
           <span
             class="block uppercase bg-gradient-to-r from-violet-900 to-pink-500 bg-clip-text text-transparent"
             >Love</span
           >
-          <span
-            class="block uppercase bg-gradient-to-r from-violet-900 to-pink-500 bg-clip-text text-transparent"
-            >Contacts</span
-          >
+          <span class="block">Contacts</span>
         </h1>
-      </div>
-
-      <div
-        class="relative w-1/2 h-[450px] md:w-full transition-all duration-500 md:inline lg:h-[300px] lg:w-[300px]"
-      >
-        <img src="../assets/heart.gif" layout="fill" objectFit="contain" />
       </div>
     </section>
     <div class="static h-[200vh]"></div>
